0b8eca8af6d0719a407cab2912fb94844511906a
arek99 <arecino@gmail.com>
2021-05-29 15:09:26 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -8,7 +8,7 @@ CREATE TABLE inventory (
 
 INSERT INTO inventory (id, name, quantity) VALUES (1, 'banana', 150); 
 INSERT INTO inventory (id, name, quantity) VALUES (2, 'orange', 154);
-INSERT INTO inventory (id, name, quantity) VALUES (2, 'apple', 160);
-INSERT INTO inventory (id, name, quantity) VALUES (2, 'lemon', 155);
-INSERT INTO inventory (id, name, quantity) VALUES (2, 'peach', 160);
-INSERT INTO inventory (id, name, quantity) VALUES (2, 'melon', 161);
+INSERT INTO inventory (id, name, quantity) VALUES (3, 'apple', 160);
+INSERT INTO inventory (id, name, quantity) VALUES (4, 'lemon', 155);
+INSERT INTO inventory (id, name, quantity) VALUES (5, 'peach', 160);
+INSERT INTO inventory (id, name, quantity) VALUES (6, 'melon', 161);
